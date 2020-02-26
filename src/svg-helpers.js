8ca9b3d877c6b9b493c293b0cf99d345de1135e0
Bartosz Lorek <bartosz.lorek@brainly.com>
2020-02-26 16:08:36 +0100

    Refactor Helpers to Operate on Spec (#1)
    * helpers with test
    
    * support fill attribute
    
    * create animate element function

--- src/svg-helpers.js
+++ src/svg-helpers.js
@@ -4,64 +4,127 @@ const options = {
   xmlMode: true
 };
 
-function getAnimatedPaths(selector, html) {
+function getAnimatedElements(selector, html, spec) {
   const $ = cheerio.load(html, options);
-  const animatedPaths = [];
+  const supportedElements = Object.keys(spec);
+  const elements = [];
 
-  $(`[id*='${selector}']`).each(function() {
-    const $elem = $(this);
-    animatedPaths.push({
-      id: $elem.attr('id'),
-      def: $elem.attr('d')
-    });
+  if (!supportedElements.length) {
+    return elements;
+  }
+
+  $(`[id*='${selector}']`).each((index, elem) => {
+    const {name, attribs} = elem;
+
+    if (supportedElements.includes(name)) {
+      const attrs = spec[name].reduce((result, attr) => {
+        if (attribs[attr] !== undefined) {
+          result[attr] = attribs[attr];
+        }
+        return result;
+      }, {});
+
+      elements.push({
+        id: attribs.id,
+        element: name,
+        attrs,
+      });
+    }
   });
-  return animatedPaths;
+
+  return elements;
 }
 
-function replaceAnimatedPaths(html, combinedPaths) {
+function mergeFrameElements(frames) {
+  // merged frames should not contain any references to other frames
+  const mergedFrame = frames[0].map(cloneElement);
+
+  // convert attributes to array and create references
+  const mergedFrameAttrsRefs = mergedFrame.reduce((refs, elem) => {
+    Object.keys(elem.attrs).forEach(attr => {
+      elem.attrs[attr] = [elem.attrs[attr]];
+    });
+    return refs.set(elem.id, elem.attrs);
+  }, new Map);
+
+  for (let i = 1; i < frames.length; i++) {
+    frames[i].forEach(elem => {
+      const ref = mergedFrameAttrsRefs.get(elem.id);
+
+      if (ref !== undefined) {
+        Object.keys(elem.attrs).forEach(attr => {
+          if (ref[attr] !== undefined) {
+            ref[attr].push(elem.attrs[attr]);
+          }
+        });
+      }
+    });
+  }
+
+  return mergedFrame;
+}
+
+function alternateFrameElements(mergedElements) {
+  mergedElements.forEach(elem => {
+    Object.keys(elem.attrs).forEach(attr => {
+      const value = elem.attrs[attr];
+
+      let index = value.length - 1;
+      while (--index >= 0) {
+        value.push(value[index]);
+      }
+    });
+  });
+
+  return mergedElements;
+}
+
+function animateFrameElements(html, mergedElements) {
   const $ = cheerio.load(html, options);
 
-  Object.keys(combinedPaths).forEach(id => {
-    $(`#${id}`).prepend(
-      `<animate
-        attributeName="d"
-        repeatCount="indefinite"
-        dur="2s"
-        values="${combinedPaths[id]}">
-      </animate>`
-    );
+  mergedElements.forEach(elem => {
+    const {id, attrs} = elem;
+    const $elem = $(`#${id}`);
+
+    if (!$elem.length) {
+      return;
+    }
+    const children = Object.keys(attrs).reduce((string, attr) => {
+      if (attrs[attr].length > 1) {
+        string += createAnimateElement({
+          attributeName: attr,
+          values: attrs[attr].join(';'),
+          repeatCount: 'indefinite',
+          dur: '2s', // todo: duration
+          begin: '0s' // todo: delay
+        });
+      }
+      return string;
+    }, '');
+
+    $elem.prepend(children);
   });
+
   return $.xml();
 }
 
-function combineAnimatedPaths(result, animatedPaths) {
-  for (let i = 0; i < animatedPaths.length; i++) {
-    const {id, def} = animatedPaths[i];
-
-    if (result[id] === undefined) {
-      result[id] = def;
-    } else {
-      result[id] += `;${def}`;
-    }
-  }
-  return result;
+function cloneElement(elem) {
+  const clone = Object.assign({}, elem);
+  clone.attrs = Object.assign({}, elem.attrs);
+  return clone;
 }
 
-function alternateAnimatedPaths(combinedPaths) {
-  Object.keys(combinedPaths).forEach(id => {
-    const def = combinedPaths[id];
-    const parts = def.split(';');
-
-    parts.pop();
-    parts.reverse();
-    combinedPaths[id] = def + ';' + parts.join(';');
-  });
-  return combinedPaths;
+function createAnimateElement(attrs) {
+  const mergedAttrs = Object.keys(attrs)
+    .map(name => `${name}="${attrs[name]}"`)
+    .join(' ');
+  return `<animate ${mergedAttrs}></animate>`;
 }
 
 module.exports = {
-  getAnimatedPaths,
-  replaceAnimatedPaths,
-  combineAnimatedPaths,
-  alternateAnimatedPaths
+  cloneElement,
+  getAnimatedElements,
+  mergeFrameElements,
+  alternateFrameElements,
+  animateFrameElements
 };
