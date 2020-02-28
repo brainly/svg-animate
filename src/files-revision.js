705067c7da7baf8ee73cde44d423877507559c4b
Bartosz Lorek <bartosz.lorek@brainly.com>
2020-02-28 16:02:47 +0100

    rename

--- src/files-revision.js
+++ src/files-revision.js
@@ -5,22 +5,22 @@ class FilesRevision {
   }
 
   getChangedFiles(compilation) {
-    const filePaths = Array.from(compilation.fileTimestamps.keys());
+    const existingFiles = Array.from(compilation.fileTimestamps.keys());
 
-    const changedFiles = filePaths.filter(path => {
+    const changedFiles = existingFiles.filter(path => {
       const currentTimestamp = compilation.fileTimestamps.get(path) || Infinity;
       return (this.prevTimestamps.get(path) || this.startTime) < currentTimestamp;
     });
 
     this.prevTimestamps = compilation.fileTimestamps;
-    const fileDependencies = Array.from(compilation.fileDependencies);
+    const dependency = Array.from(compilation.fileDependencies);
 
     // for initial build
     if (changedFiles.length === 0) {
-      return fileDependencies;
+      return dependency;
     }
 
-    return changedFiles.filter(file => fileDependencies.includes(file));
+    return changedFiles.filter(file => dependency.includes(file));
   }
 }
 
